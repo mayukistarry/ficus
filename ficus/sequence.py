432419a0ef927dc32972e3eb9d79937beb57f518
mayukistarry <mayukistarry@gmail.com>
2023-11-04 18:22:31 +0900

    数列を追加

---
+++ ficus/sequence.py
@@ -0,0 +1,55 @@
+class ProportionalSequence:
+    def __init__(self, a, b, size):
+        self.__a = a
+        self.__b = b
+        self.__size = size
+        self.__items = []
+        for x in range(self._size):
+            self.__items.append(self.__a * x + self.__b)
+    
+    @property
+    def items(self):
+        return self.__items
+
+class InverseProportinalSequence:
+    def __init__(self, a, b, size):
+        self.__a = a
+        self.__b = b
+        self.__size = size
+        self.__items = []
+        for x in range(self._size):
+            w = x + 1
+            self.__items.append(self.__a / w + self.__b)
+        
+    @property
+    def items(self):
+        return self.__items
+
+class QuadraticSequence:
+    def __init__(self, a, b, c, size):
+        self.__a = a
+        self.__b = b
+        self.__c = c
+        self.__size = size
+        self.__items = []
+        for x in range(self._size):
+            self.__items.append(self.__a * x ** 2 + self.__b * x + self.__c)
+    
+    @property
+    def items(self):
+        return self.__items
+
+class InverseQuadraticSequence:
+    def __init__(self, a, b, c, size):
+        self.__a = a
+        self.__b = b
+        self.__c = c
+        self.__size = size
+        self.__items = []
+        for x in range(self._size):
+            w = x + 1
+            self.__items.append(self.__a / w ** 2 + self.__b / w + self.__c)
+    
+    @property
+    def items(self):
+        return self.__items
