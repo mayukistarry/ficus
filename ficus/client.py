952fa6868e51f7d79d4454cfc2a1cf3483baca37
mayukistarry <mayukistarry@gmail.com>
2023-11-06 02:07:45 +0900

    status網羅処理追加

--- ficus/client.py
+++ ficus/client.py
@@ -1,2 +1,63 @@
+#上記2つはある程度のこす
+#再帰の方法・こちらの方が行数は少ないが、再帰使用しないのも思ったよりもわかりやすいのでこちらは採用しない
+def __combine_v1(lists, prefix=[]):
+    if not lists:
+        # もう組み合わせるリストがない場合、これまでの組み合わせを返す
+        return [prefix]
+    else:
+        # 組み合わせるリストがまだある場合、再帰を利用して組み合わせを作る
+        result = []
+        for item in lists[0]:
+            result.extend(combine(lists[1:], prefix + [item]))
+        return result
+
+#下記でいいはず・検証は必要
+def __combine_v2(lists):
+    if not lists:
+        return []
+    
+    # スタックに最初の要素を初期状態として追加
+    stack = [[item] for item in lists[0]]
+    result = []
+    
+    while stack:
+        current = stack.pop()
+        # 現在の組み合わせの長さが元のリストの長さと等しい場合、結果に追加
+        if len(current) == len(lists):
+            result.append(current)
+        else:
+            # 現在の組み合わせに次の要素を追加して新しい組み合わせを作る
+            next_list = lists[len(current)]
+            for item in next_list:
+                new_combination = current + [item]
+                stack.append(new_combination)
+    
+    return result
+
+
+
 class Client:
-    pass
+    def __init__(self):
+        self.__items = {}
+
+    def __combine(self, lists):
+        if not lists:
+            return []
+    
+        # スタックに最初の要素を初期状態として追加
+        stack = [[item] for item in lists[0]]
+        result = []
+        
+        while stack:
+            current = stack.pop()
+            # 現在の組み合わせの長さが元のリストの長さと等しい場合、結果に追加
+            if len(current) == len(lists):
+                result.append(current)
+            else:
+                # 現在の組み合わせに次の要素を追加して新しい組み合わせを作る
+                next_list = lists[len(current)]
+                for item in next_list:
+                    new_combination = current + [item]
+                    stack.append(new_combination)
+        
+        return result
