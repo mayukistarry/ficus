2b9e053f4924b5aa319ae2259f2db3f3565e3f10
mayukistarry <mayukistarry@gmail.com>
2023-11-06 01:23:58 +0900

    複数ステータス管理を追加

--- ficus/status.py
+++ ficus/status.py
@@ -1,5 +1,19 @@
 from ficus.row import RowInterface
 
+class Statuses:
+    def __init__(self, statuses):
+        self.__statuses = {}
+        for status in statuses:
+            if status.column_name in self.__statuses:
+                self.__statuses[status.column_name].append(status)
+            else:
+                self.__statuses[status.column_name] = [status]
+    
+    @property
+    def items(self):
+        return self.__statuses
+
+
 class Status(RowInterface):
     def __init__(self, column_name, status_name, weight):
         self.__column_name = column_name
@@ -13,4 +27,3 @@ class Status(RowInterface):
     @property
     def column_name(self):
         return self.__column_name
-    
